49fe1bf235d9d241beb6cdff4b0c4ed5396f9ac0
ScribblesUrsa <leiscribbles@gmail.com>
2023-08-15 17:41:53 -0600

    Added Randomizer for slot characters

--- Program.cs
+++ Program.cs
@@ -4,14 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
-            Console.WriteLine("Slot Machine-Very Bad Gambling Habit");
-            Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
+            Random slotCharacter = new Random();
 
-            List<char> reelCharacters = new List<char>()                                                        //List of Slot Machine Character per Column/Reel
+            int slotIndex = 0;
+
+            List<char> reelCharacters = new List<char>()                                                            //List of Slot Machine Character per Column/Reel
             {
             'A', '1', '7', '$', '*', '8', '9', '!', '#', '^', '%', '+', 'C', 'S', 'Y', 'V', 'W', 'R', 'L', 'F'
             };
+
+            Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
+            Console.WriteLine("Slot Machine-Very Bad Gambling Habit");
+            Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");   
+            
+            slotIndex = slotCharacter.Next(reelCharacters.Count);
+
+            char[,] slots = new char[3, 3];                                                                         //Defining the slot matrices of the game
         }
     }
 }
