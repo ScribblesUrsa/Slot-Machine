453336ad9dca7177544c7b3a1243b1670a566a6b
ScribblesUrsa <leiscribbles@gmail.com>
2023-08-14 21:46:22 -0600

    First List/Array of the 2D multi array

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@
             Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
             Console.WriteLine("Slot Machine-Very Bad Gambling Habit");
             Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
+
+            List<char> reelCharacters = new List<char>()                                                        //List of Slot Machine Character per Column/Reel
+            {
+            'A', '1', '7', '$', '*', '8', '9', '!', '#', '^', '%', '+', 'C', 'S', 'Y', 'V', 'W', 'R', 'L', 'F'
+            };
         }
     }
 }
