92ce00c1764489dcfa2ca2621b2301fad5b759df
ScribblesUrsa <leiscribbles@gmail.com>
2023-08-15 18:04:58 -0600

    Added Constants

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 {
     internal class Program
     {
+        const double PLAY_PER_LINE = 1.5;                                                                                //Either Horizonal, Vertical or DIagonal, does not matter
+        const double PLAY_ALL_HORIZONTAL = 4;
+        const double PLAY_ALL_VERTICAL = 4;
+        const double PLAY_ALL_DIAGONAL = 2.5;
+        const double PLAY_ALL = 9;
+        const double SINGLE_LINE_HORIZONTAL_VERTICAL_DIAGONAL_PRIZE = 5;
+        const double DOUBLE_LINE_HORIZONAL_VERTICAL_DIAGONAL_PRIZE = 15;
+        const double TRIPLE_LINE_PRIZE = 30;
+
         static void Main(string[] args)
         {
             Random slotCharacter = new Random();
