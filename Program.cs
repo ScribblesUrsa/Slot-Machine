0b997f4f50c18ab7f06824d62d1315335e6e30a6
ScribblesUrsa <leiscribbles@gmail.com>
2023-08-17 18:19:43 -0600

    Added Characters to Slots

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Slot_Machine
+﻿using System.ComponentModel;
+
+namespace Slot_Machine
 {
     internal class Program
     {
+        const int X = 3;
+        const int Y = 3;
         const double PLAY_PER_LINE = 1.5;                                                                                //Either Horizonal, Vertical or DIagonal, does not matter
         const double PLAY_ALL_HORIZONTAL = 4;
         const double PLAY_ALL_VERTICAL = 4;
@@ -18,19 +22,40 @@
 
             List<char> reelCharacters = new List<char>()                                                                //List of Slot Machine Character per Column/Reel
             {
-            'A', '1', '7', '$', '*', '8', '9', '!', '#', '^', '%', '+', 'C', 'S', 'Y', 'V', 'W', 'R', 'L', 'F'
+            'A', '1',  '5', '7', '$', 'M', '8', '9', '!', '#', 'Q', '%', 'C', 'S', 'Y', 'V', 'W', 'R', 'L', 'F'
             };
+
+            List<char> characterHolder = new List<char>();                                                              //To Store Character Items Temporarily and placed into the slots    
 
             Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
             Console.WriteLine("Slot Machine-Very Bad Gambling Habit");
-            Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");   
-            
-            char[,] slots = new char[3, 3];                                                                             //Defining the slot matrices of the game
+            Console.WriteLine("o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o");
+            Console.WriteLine("\n");
+
+            char[,] slots = new char[X, Y];                                                                             //Defining the slot matrices of the game
 
             foreach (char slot in slots)
             {
                 slotIndex = slotCharacter.Next(reelCharacters.Count);
+                characterHolder.Add(reelCharacters[slotIndex]);
             }
+
+            slots[0, 0] = characterHolder[0];
+            slots[0, 1] = characterHolder[1];
+            slots[0, 2] = characterHolder[2];
+            slots[1, 0] = characterHolder[3];
+            slots[1, 1] = characterHolder[4];
+            slots[1, 2] = characterHolder[5];
+            slots[2, 0] = characterHolder[6];
+            slots[2, 1] = characterHolder[7];
+            slots[2, 2] = characterHolder[8];
+
+            Console.WriteLine(slots[0, 0] + " - " + slots[0, 1] + " - " + slots[0, 2]);
+            Console.WriteLine();
+            Console.WriteLine(slots[1, 0] + " - " + slots[1, 1] + " - " + slots[1, 2]);
+            Console.WriteLine();
+            Console.WriteLine(slots[2, 0] + " - " + slots[2, 1] + " - " + slots[2, 2]);
+
         }
     }
 }
